14d5f7e56490c93cf275403671f7829825d279db
Mahesh Warang <maheshwarang001@gmail.com>
2024-02-24 14:16:15 +0000

    first

---
+++ src/main/java/com/example/md_backend/MdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.md_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/md_backend/MdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
