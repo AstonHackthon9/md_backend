7e097783c4ff03b81decc5c6e40feafd3ef18da2
Mahesh Warang <maheshwarang001@gmail.com>
2024-02-25 02:03:02 +0000

    schedule webrtc

---
+++ src/main/java/com/example/md_backend/websocket/WebSocketConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.md_backend.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.handler.PerConnectionWebSocketHandler;
+
+import java.io.IOException;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer  {
+
+
+    @Autowired
+    SocketHandler socketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+        registry.addHandler(socketHandler, "/socket")
+                    .setAllowedOrigins("*");
+
+    }
+}
